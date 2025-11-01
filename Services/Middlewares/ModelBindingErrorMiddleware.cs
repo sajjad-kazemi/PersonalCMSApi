21c9aa596af327acd7d8baa0ffc237d034897638
Saji <sajjadkazemiswe@gmail.com>
2025-11-01 10:08:35 +0330

    middleware

---
+++ Services/Middlewares/ModelBindingErrorMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace CMSAPI.Middlewares
+{
+	public class ModelBindingErrorMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly ILogger<ModelBindingErrorMiddleware> _logger;
+		public ModelBindingErrorMiddleware(
+			RequestDelegate next,
+			ILogger<ModelBindingErrorMiddleware> logger)
+		{
+			_next = next;
+			_logger = logger;
+		}
+		public async Task InvokeAsync(HttpContext context)
+		{
+			try
+			{
+				await _next(context);
+			}
+			catch (JsonException ex)
+			{
+				_logger.LogWarning(ex , "JSON building error");
+				context.Response.StatusCode = StatusCodes.Status400BadRequest;
+				var message = $"درخواست وارد شده با فرمت اشتباه ارسال شده است. برای بخش `{ex.Path}`: {ex.Message}";
+				var errorObj = new
+				{
+					status = 400 ,
+					error = message
+				};
+
+				await context.Response.WriteAsJsonAsync(errorObj);
+				return;
+			}
+		}
+	}
+}
